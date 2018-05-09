bf04f119ecc9156fc4f79581799b930a5c8c8f1d
Edmund <edmundcjj@gmail.com>
2018-05-09 10:32:01 +0800

    Moved portions of code into function

--- converter-program.js
+++ converter-program.js
@@ -1,33 +1,32 @@
-
 var jsonfile = require('jsonfile');
 var fs = require('fs');
 
-if (process.argv[2]) {
-  var file = process.argv[2];
+function read_file() {
+  if (process.argv[2]) {
+    var file = process.argv[2];
+  } else {
+    var file = 'countries.json';
+  }
+
+  jsonfile.readFile(file, function(err, obj) {
+    let countries_array = obj.countries;
+
+    convert_to_sql_insert(countries_array);
+  })
 }
-else {
-  var file = 'countries.json';
-}
-jsonfile.readFile(file, function(err, obj) {
-  let countries_array = obj.countries;
 
-  convert_to_sql_insert(countries_array);
-})
-
-
-function convert_to_sql_insert(countries){
+function convert_to_sql_insert(countries) {
   var output_file = 'countries.sql';
 
-  for(var country of countries) {
-    for(var details in country) {
+  for (var country of countries) {
+    for (var details in country) {
       if (process.argv[3]) {
-        var insert_statement = "INSERT INTO " + process.argv[3] +" (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
-      }
-      else {
+        var insert_statement = "INSERT INTO " + process.argv[3] + " (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
+      } else {
         var insert_statement = "INSERT INTO countries (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
       }
-      fs.appendFile(output_file, insert_statement ,function(err){
-        if(err)
+      fs.appendFile(output_file, insert_statement, function(err) {
+        if (err)
           console.error(err);
         console.log('Appended!');
       });
@@ -35,3 +34,5 @@ function convert_to_sql_insert(countries){
   }
   console.log("Finished writing all sql statements to sql file");
 }
+
+read_file();
