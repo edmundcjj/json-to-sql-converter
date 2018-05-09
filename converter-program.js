90992f773c64dc38dcc4783f90cb7203c9c57a90
Edmund <edmundcjj@gmail.com>
2018-05-09 10:43:40 +0800

    Edited file

--- converter-program.js
+++ converter-program.js
@@ -19,20 +19,13 @@ function convert_to_sql_insert(countries) {
   var output_file = 'countries.sql';
 
   for (var country of countries) {
-    for (var details in country) {
-      if (process.argv[3]) {
-        var insert_statement = "INSERT INTO " + process.argv[3] + " (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
-      } else {
-        var insert_statement = "INSERT INTO countries (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
-      }
-      fs.appendFile(output_file, insert_statement, function(err) {
-        if (err)
-          console.error(err);
-        console.log('Appended!');
-      });
-    }
+    var insert_statement = "INSERT INTO " + process.argv[3] + " (name, code) VALUES ('" + country["name"] + "','" + country["code"] + "');\n";
+    fs.appendFile(output_file, insert_statement, function(err) {
+      if (err)
+        console.error(err);
+      console.log('Appended!');
+    });
   }
-  console.log("Finished writing all sql statements to sql file");
 }
 
 read_file();
